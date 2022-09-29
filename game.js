ee1cd072e4bcea35b17c68f7bca34a1c73b14f2f
e-lamine <lamine@e-lamine.dev>
2022-09-29 09:16:45 +0100

    finished the assignment by adding game function the game works but i'll try to refactor later

--- game.js
+++ game.js
@@ -12,39 +12,88 @@ if (n === 0) {
 } else { return 'something is wrong';}
 }
 
-const playerSelection = prompt().toLowerCase();
-const computerSelection = getComputerChoice();
+
+
+// let playerWins = `You win! ${playerSelection} beats ${computerSelection}`;
+// let computerWins = `You loose! ${computerSelection} beats ${playerSelection}`;
+// let tie = "Tie, lets play again";
+
+let roundWinner = "";
 
 
 function playRound(playerSelection, computerSelection) {
+
+    
     
     if (playerSelection === 'rock' && computerSelection === 'rock' || 
         playerSelection === 'paper' &&  computerSelection === 'paper' || 
         playerSelection === 'scissors' && computerSelection === 'scissors'){ 
-            return "Tie, lets play again";
+            
+            roundWinner = 'tie';
 
-        }else if (playerSelection === 'rock' && computerSelection === 'scissors' || 
+        } else if (playerSelection === 'rock' && computerSelection === 'scissors' || 
              playerSelection === 'scissors' &&  computerSelection === 'paper' || 
              playerSelection === 'paper' && computerSelection === 'rock') { 
-                return `You win! ${playerSelection} beats ${computerSelection}`
+
+                roundWinner = 'player';
 
         } else if (playerSelection === 'scissors' && computerSelection === 'rock' || 
              playerSelection === 'paper' &&  computerSelection === 'scissors' || 
              playerSelection === 'rock' && computerSelection === 'paper') {
-                return `You loose! ${computerSelection} beats ${playerSelection}`;
+                
+                roundWinner = 'computer';
         
         } else { return "there must be some bug"}
               
   }
 
+
+
+function game(){
+
+    let computerScore = 0;
+    let playerScore = 0;
+    let ties = 0;
+
+
+    for (let i = 0; i < 5; i++) {
+
+        const playerSelection = prompt().toLowerCase();
+        const computerSelection = getComputerChoice();
+
+        playRound(playerSelection, computerSelection);
+        if (roundWinner === 'player') { playerScore++ 
+        } else if (roundWinner === 'computer') { computerScore++ 
+        } else if (roundWinner === 'tie') { ties++
+        } else { console.loge('something is not working')}
+
+        
+        console.log(playerSelection);
+        console.log(computerSelection); 
+        console.log(playerScore);
+        console.log(computerScore);       
+    }
+
+    if (playerScore > computerScore) {
+        console.log("you won!")
+    } else if (computerScore > playerScore) {
+        console.log('you lost')
+    } else if (computerScore === playerScore) {
+        console.log("It's a tie")
+    }  
+}
  
-  console.log(playerSelection);
-  console.log(computerSelection);
-  console.log(playRound(playerSelection, computerSelection));
 
 
-//   `You win! ${playerSelection} beats ${computerSelection}`;
-//   { return `You loose! ${computerSelection} beats ${playerSelection}`;
+
+
+
+//   console.log(playerSelection);
+//   console.log(computerSelection);
+//   console.log(playRound(playerSelection, computerSelection));
+
+
+
 
   
 
