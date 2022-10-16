a9818cca4fccdbc71d182883ff06a7ff82e7c820
e-lamine <lamine@e-lamine.dev>
2022-10-16 09:57:50 +0100

    first draft

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+// const prompt = require("prompt-sync")({ sigint: true });
+
 
 function getComputerChoice(){
 
@@ -10,88 +12,125 @@ if (n === 0) {
 } else if (n === 2) {
     return 'scissors';
 } else { return 'something is wrong';}
-}
+};
 
-
-
-// let playerWins = `You win! ${playerSelection} beats ${computerSelection}`;
-// let computerWins = `You loose! ${computerSelection} beats ${playerSelection}`;
-// let tie = "Tie, lets play again";
 
 let roundWinner = "";
-
+let computerScore = 0;
+let playerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
 
-    
-    
+
     if (playerSelection === 'rock' && computerSelection === 'rock' || 
         playerSelection === 'paper' &&  computerSelection === 'paper' || 
         playerSelection === 'scissors' && computerSelection === 'scissors'){ 
             
             roundWinner = 'tie';
 
-        } else if (playerSelection === 'rock' && computerSelection === 'scissors' || 
-             playerSelection === 'scissors' &&  computerSelection === 'paper' || 
-             playerSelection === 'paper' && computerSelection === 'rock') { 
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors' || 
+               playerSelection === 'scissors' &&  computerSelection === 'paper' || 
+               playerSelection === 'paper' && computerSelection === 'rock') { 
 
                 roundWinner = 'player';
 
-        } else if (playerSelection === 'scissors' && computerSelection === 'rock' || 
-             playerSelection === 'paper' &&  computerSelection === 'scissors' || 
-             playerSelection === 'rock' && computerSelection === 'paper') {
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock' || 
+               playerSelection === 'paper' &&  computerSelection === 'scissors' || 
+               playerSelection === 'rock' && computerSelection === 'paper') {
                 
                 roundWinner = 'computer';
         
-        } else { return "there must be some bug"}
-              
+    } else { return "there must be some bug" }  
+    
+    return roundWinner;
   }
 
 
 
-function game(){
 
-    let computerScore = 0;
-    let playerScore = 0;
-    let ties = 0;
+  let selectionButtons = document.querySelectorAll('.selectBtn');
 
+  selectionButtons.forEach(btn => {
 
-    for (let i = 0; i < 5; i++) {
-
-        const playerSelection = prompt().toLowerCase();
-        const computerSelection = getComputerChoice();
-
+      btn.addEventListener('click', function () {
+        let playerSelection = btn.id
+        let computerSelection = getComputerChoice();
         playRound(playerSelection, computerSelection);
-        if (roundWinner === 'player') { playerScore++ 
-        } else if (roundWinner === 'computer') { computerScore++ 
-        } else if (roundWinner === 'tie') { ties++
-        } else { console.loge('something is not working')}
+        countScore(roundWinner);
+        declareWinner(playerScore, computerScore);
 
-        
-        console.log(playerSelection);
-        console.log(computerSelection); 
-        console.log(playerScore);
-        console.log(computerScore);       
+        console.log(playerSelection, computerSelection, roundWinner );
+      });
+      
+  });
+
+  let roundWinnerText = document.getElementById('roundWinner');
+  let gameWinnerText = document.getElementById('gameWinner');
+  let selectionButtonsDiv = document.getElementById('selectionButtons');
+
+
+  
+function countScore(roundWinner) {
+    if (roundWinner === 'player') { playerScore++ ; roundWinnerText.innerText = 'you won this round!';
+                } else if (roundWinner === 'computer') { computerScore++; roundWinnerText.innerText = 'computer won this round'
+                } else if (roundWinner === 'tie') { ties++ ,roundWinnerText.innerText = 'no one won this round'
+                } 
+    console.log(computerScore, playerScore);
+    return (computerScore, playerScore)
+}
+
+ 
+function declareWinner(computerScore,playerScore) {
+
+    let gameWinner = ""
+    if (computerScore === 5) { gameWinner = "computer" ; roundWinnerText.innerText = 'game over the computer won'
+    }else if (playerScore === 5) { gameWinner = "player" ; roundWinnerText.innerText = 'you won! congrats'}
+
+    if (gameWinner){
+
+        selectionButtonsDiv.remove();
+
     }
 
-    if (playerScore > computerScore) {
-        console.log("you won!")
-    } else if (computerScore > playerScore) {
-        console.log('you lost')
-    } else if (computerScore === playerScore) {
-        console.log("It's a tie")
-    }  
 }
+
+
+
+// function game(){
+
+//     let computerScore = 0;
+//     let playerScore = 0;
+//     let ties = 0;
+
+
+//     for (let i = 0; i < 5; i++) {
+
+//         const playerSelection = prompt().toLowerCase();
+//         const computerSelection = getComputerChoice();
+
+//         playRound(playerSelection, computerSelection);
+//         if (roundWinner === 'player') { playerScore++ 
+//         } else if (roundWinner === 'computer') { computerScore++ 
+//         } else if (roundWinner === 'tie') { ties++
+//         } else { console.loge('something is not working')}
+  
+//         console.log(playerSelection);
+//         console.log(computerSelection); 
+//         console.log(playerScore);
+//         console.log(computerScore);       
+//     }
+
+//     if (playerScore > computerScore) {
+//         console.log("you won!")
+//     } else if (computerScore > playerScore) {
+//         console.log("you lost")
+//     } else if (computerScore === playerScore) {
+//         console.log("It's a tie")
+//     }  
+// }
  
 
-
-
-
-
-//   console.log(playerSelection);
-//   console.log(computerSelection);
-//   console.log(playRound(playerSelection, computerSelection));
-
+// game();
 
 
 
